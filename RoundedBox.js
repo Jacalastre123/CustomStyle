8b0ec7237da979b8c4ca92cdce1a28032d9f42dd
BrowWebs <wtran786@gmail.com>
2026-01-04 13:08:42 +1100

    Revise console log and add styling instructions
    Updated console log message and added comments on styling.

--- RoundedBox.js
+++ RoundedBox.js
@@ -13,5 +13,17 @@ allElements.forEach(value => {
   }
       
 })
-console.log("Welcome! Go to https://jacalastre123.github.io/customStyle!")
-  
+console.log("Welcome! Go to https://jacalastre123.github.io/customStyle if you want the same for your code!")
+
+// This above is how I make your custom tags like <box-red> actually red!
+
+//If you want to do something similar without the boredom, here's how:
+
+// HTML Body: Structure + <div class="red"><h3>Whatever here</h3></div> (The container to colour it, the class is the identifier)
+//CSS:
+// .red {
+// background-color: red; (Your background color (Any colour or rgb here!)
+// border-radius: 4.5px; (The curves on the edges)
+// padding: 5px; (The extra space after the contents) or padding: 15px (if you want more padding AKA the high part)
+// } 
+// By Jacalastre123 
