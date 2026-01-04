b47fff61017bacd82d9d0297604c66d1b99cae5b
BrowWebs <wtran786@gmail.com>
2026-01-04 12:10:44 +1100

    Add padding to box elements in RoundedBox.js

--- RoundedBox.js
+++ RoundedBox.js
@@ -4,6 +4,8 @@ allElements.forEach(value => {
   if (value.tagName.toLowerCase().startsWith("box-")) {
     let splits = value.tagName.split("-")
     value.style.backgroundColor = splits[1]
+    value.style.padding = "5px"
+    
     if (splits[2] === "high") {
       value.style.padding = "15px"
     }
