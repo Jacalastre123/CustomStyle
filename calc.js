90af9eec6c37ca9f05ab3dcaecca8aa2fa344d96
BrowWebs <wtran786@gmail.com>
2026-02-07 14:47:22 +1100

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,76 +1,180 @@
 let codes = [
-    "#", "---", "|", "-", "##", "___", "*LIST", "*", "<=" // Code list
+    "#", "---", "|", "-", "##", "___", "*LIST", "*", "<=", "###", "-+-", "/+/"
 ]
-const container = document.getElementById("container") // Get your container ID
-let inTable = false // Is in table?
+let container = document.getElementById("container")
+let inTable = false
 let inTh = false
 let table;
 let tHead;
 let lineArray = []
 let tRow;
 let tRow2;
-let ul; // Bunch of let statements for more universal access to other commands like lists and tables
+let ul;
+let menu;
+let selection;
+let newList;
+let newSub;
+let friction = 0
 let colList = []
 function loader(post) {
     container.innerHTML = ""
-fetch(post + ".wtd") // Fetch your post
+fetch(post + ".wtd")
 .then(response => response.text())
 .then(data => {
-    const lines = data.split(/\r?\n/) // Splits it by newl ine
+    const lines = data.split(/\r?\n/)
 
-    let cmd; // Command
+    let cmd;
 
     lines.forEach(line => {
 
-        const part = line.split(" ") //Split it
-         cmd = part[0] // Command part
+        const part = line.split(" ")
+         cmd = part[0]
       
-        const args = part.slice(1).join(" ") // Rest
+        const args = part.slice(1).join(" ")
         
         if (codes.includes(cmd)) {
-            switch(cmd) { // Switch for many ifs
+            switch(cmd) {
 
-                case "___": // <hr>
+                case "___":
                    
                     container.appendChild(document.createElement("hr"))
                     break;
+
+                case "-+-":
+                    let classing = args.trim()
+         
+                     menu = document.createElement("div")
+                                if (classing !== "") {
+                        menu.className = classing
+                    }
+                   menu.innerHTML = "<h4>Table of Contents: </h4><hr>"
+                   selection = container.querySelectorAll("h3")
+                   container.appendChild(menu)
+               
+                  
+                    
+                    break;
                 
                
-                case "##": // <h4>
-                    const subheadline = document.createElement("h3") // Create element
-                    subheadline.innerText = args // Put text
-                    container.appendChild(subheadline) // Put it to container
-                    break; // End case
+               
+                     case "##":
+                    const subheadline = document.createElement("h2")
+                    subheadline.innerText = args
+                    container.appendChild(subheadline)
+                    break;
 
-                 case "<=": // Assign text to ID
+
+
+
+                
+                case "/+/":
+                    selection = container.querySelectorAll("h2, h3")
+
+                  
+                   selection.forEach((item, index) => {
+                    
+                   
+
+                        if (item.tagName === "H2") {       
+                        newList = document.createElement("h4")
+                          newList.onclick = function() {
+                        item.scrollIntoView({
+                            behavior: "smooth",
+                            block: "start"
+                        })
+                        
+
+                    }
+                       newList.innerText = item.textContent
+newList.style.padding = "10px"
+                        
+
+                        newList.style.backgroundColor = "lightgrey"
+                               
+                              
+                   
+                        
+                    menu.appendChild(newList)
+                }
+
+                        if (item.tagName === "H3") {  
+                            friction++     
+                    newSub = document.createElement("h4")
+                        
+                         
+              
+                        newSub = document.createElement("h5")
+                          newSub.onclick = function() {
+                        item.scrollIntoView({
+                            behavior: "smooth",
+                            block: "start"
+                        })
+                       
+                            
+                        }
+                         newSub.style.padding = "2px"
+                        newSub.style.backgroundColor = "lightgrey"
+                         newSub.innerText = item.textContent
+                          menu.appendChild(newSub)
+                        
+                        
+                    }}
+                    )
+                   
+
+                    
+                  
+
+                    
+
+                
+                
+                    break;
+
+                case "###":
+                    const subheadline2 = document.createElement("h3")
+                    subheadline2.innerText = args
+                    container.appendChild(subheadline2)
+                    break;
+
+                 case "<=":
                     let argsSplit1 = args.split(" ")
                     let argsSplit2 = argsSplit1[0]
-                   
-                    
+                   if (line.includes("(div)")) {
+                    container = document.getElementById(argsSplit2)
+                   }
+
+
+                   else if (line.includes("(END)")) {
+                    container = document.getElementById("container")
+                   }
+                    else {
                     const id = document.getElementById(argsSplit2)
                     id.innerText = argsSplit1.slice(1).join(" ")
-
+                    }
                     
                     break;
 
-                case "#": // <h1>
-                    const headline = document.createElement("h2")
+                case "#":
+                    const headline = document.createElement("h1")
                     headline.innerText = args
                     container.appendChild(headline)
                     break;
 
-                case "*LIST": // <ul> only
+
+
+                case "*LIST":
                     ul = document.createElement("ul")
                     container.appendChild(ul)
                     break;
 
-                case "*": // <li>
+                case "*":
                     const li = document.createElement("li")
                     li.innerText = args
                     ul.appendChild(li)
                     break;
 
-                case "---": // <table>
+                case "---":
                     table = document.createElement("table")
                      tRow = document.createElement("tr")
                     container.appendChild(table)
@@ -79,7 +183,7 @@ fetch(post + ".wtd") // Fetch your post
 
                 
 
-                case "|": // <th> + <tr>
+                case "|":
                   let splitter =  args.split(" / ")
                   splitter.forEach(splits => {
                      tHead = document.createElement("th")
@@ -94,7 +198,7 @@ fetch(post + ".wtd") // Fetch your post
 
 
 
-                    case "-": // <tr> + <td>
+                    case "-":
                         tRow2 = document.createElement("tr")
                     table.appendChild(tRow2)
                    let splitter2 =  args.split(" / ")
@@ -114,10 +218,21 @@ fetch(post + ".wtd") // Fetch your post
         } 
        
        
-        else if (line.startsWith("pic(")) {
+         else if (line.startsWith("pic(")) {
             const imag = document.createElement("img")
                     imag.src = line.slice(4, -1)
                     container.appendChild(imag)
+        } 
+
+         else if (line.startsWith("vid(")) {
+            container.appendChild(document.createElement("br"))
+            const ivid = document.createElement("video")
+                    ivid.controls = true
+            const sources = document.createElement("sources")
+            sources.src = line.slice(4, -1)
+            sources.type = "video/" + line.slice(-4,-1).toLowerCase()
+                    container.appendChild(ivid)
+                    ivid.appendChild(sources)
         } 
          else if (line.trim() !== "" || line.includes("(SM)") || line.includes("(BD)") || line.includes("(COLOR-)")) {
                  const para = document.createElement("p")
@@ -135,6 +250,13 @@ fetch(post + ".wtd") // Fetch your post
                    
                         
                     }
+                    
+                    else {
+                            para.innerText = args
+                      
+                     }
+                            container.appendChild(para)
+                    }
 
                     else if (line.includes("(COLOR-")) {
                         let match = line.match(/\(COLOR-([^)]+)\)/)
@@ -144,6 +266,45 @@ fetch(post + ".wtd") // Fetch your post
                         }
                         para.innerText = line
                     }
+                
+                        else if (line.startsWith("[ID=")) {
+                        let match2 = line.slice(4,-1)
+                            const newParas = document.createElement("p")
+                       newParas.id = match2
+                      
+                            container.appendChild(newParas)
+                        }
+                 
+
+                     else if (line.startsWith("$")) {
+                        let match3 = line.slice(1)
+                                        if (match3.startsWith("BGCOL=")) {
+                                            container.style.backgroundColor = match3.trim().slice(6)
+                                        }
+
+                                        if (match3.startsWith("FONT=")) {
+                                            container.style.fontFamily = match3.trim().slice(5)
+                                        }
+                                        if (match3.startsWith("HCSS=")) {
+                                            let cssing = document.createElement("style")
+                                            cssing.innerHTML = match3.trim().slice(5)
+                                            document.body.appendChild(cssing) 
+                                        }
+
+                                        if (match3.startsWith("TA=")) {
+
+                                            container.style.textAlign = match3.trim().slice(3)
+                                            
+                                        }
+
+                                        else {
+                                            const parag = document.createElement("p")
+                                            parag.innerText = line
+                                            container.appendChild(parag)
+                                        }
+                      
+                         
+                    }
 
                       else if (line.startsWith("(CLASS-")) {
                         let match3 = line.match(/\(CLASS-([^)]+)\)/)
@@ -151,21 +312,36 @@ fetch(post + ".wtd") // Fetch your post
                             para.className = match3[1]
                             line = line.replace(match3[0], "").trim()
                         }
-                        para.innerText = line
-                    }
-                    else {
+                        
+                    
+                        else {
                          para.innerText = line
                     }
+                }
+line.split(" ").forEach(item => {
+                     if (item.includes("[") && item.incudes("]") && item.startsWith("'") && item.endsWith("'")) {
+                           
+                         let match3 = item.slice(indexOf("'")).slice(0, lastIndexOf("'")).substring(item.indexOf("["), item.indexOf("]"))
+                            const newA = document.createElement("a")
+                       newA.href = match3
 
-                    container.appendChild(para)
-                    
-        } 
+                         newA.innerText = item.substring(item.indexOf("]"))
+                         
+                      
+                            container.appendChild(newA)
+                    }}) 
+                
+                   
+    }
+            
+         
      
 
 
-    })
+)
     
 })
 .catch(err => console.error(err))
 
+history.replaceState(null, "", "#" + post.slice(40).replace(/\//g, "-"))
 }
